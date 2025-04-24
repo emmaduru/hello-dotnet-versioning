ded7ebbb252da4c6e0e57468861e2d3dbcce1d14
emmaduru <99149024+emmaduru@users.noreply.github.com>
2025-04-24 13:55:35 +0000

    odd-even functionality added

--- hello/Program.cs
+++ hello/Program.cs
@@ -6,6 +6,11 @@ class Program
     {
         Console.WriteLine("Enter a number");
         var number = int.Parse(Console.ReadLine());
-        Console.WriteLine("This is your number: " + number);
+
+        if (number % 2 == 0) {
+            Console.WriteLine("Number is even");
+        } else {
+            Console.WriteLine("Number is odd.");
+        }
     }
 }
